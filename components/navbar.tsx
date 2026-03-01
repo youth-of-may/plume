3c38ada5f1791ed04a26023adcc90414dd8807ab
youth-of-may <135508335+youth-of-may@users.noreply.github.com>
2026-03-01 23:26:57 +0800

    Added initial logic to edit user profile.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,8 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { createClient } from "@/utils/supabase/client";
 
 export default function Navbar() {
   return (
@@ -18,30 +20,46 @@ import { usePathname } from "next/navigation";
 
 /// FullNav
 function FullNav() {
+  const router = useRouter();
   const pathname = usePathname();
+
+  const supabase = createClient();
+
+  async function signOut() {
+    const { error } = await supabase.auth.signOut();
+
+    if (error) {
+      console.error('Sign out error:', error.message);
+      return;
+    }
+    router.push('/');
+    console.log('Successfully signed out')
+  }
+
+
 
   return (
     <div className="flex flex-col h-screen bg-[#F7F9FC] shadow-sm border-r-12 border-r-[#ADD3EA] pt-8 pb-8 gap-12">
-        <div className="flex flex-col items-center font-delius gap-4">
-            <Image src='/chiikawa.jpg' width={120} height={120} className="rounded-full border-4 border-[#4F84A5]"/>
-            <p className="text-lg font-bold">username</p>
-            <p className="text-sm">edit profile</p>
-        </div>
-        <nav className="flex flex-col items-center w-full gap-4 font-delius">
-            <NavLink href="/" label="home" active={pathname === "/"} />
-            <NavLink href="/" label="write entry" active={pathname === "/"} />
-            <NavLink href="/" label="journal archive" active={pathname === "/"} />
-            <NavLink href="/" label="calendar" active={pathname === "/"} />
-            <NavLink href="/" label="task list" active={pathname === "/"} />
-            <NavLink href="/" label="shop" active={pathname === "/"} />
-            <NavLink href="/" label="gacha" active={pathname === "/"} />
-            <NavLink href="/" label="dashboard" active={pathname === "/"} />
-            <NavLink href="/" label="inventory" active={pathname === "/"} />
-            <NavLink href="/" label="item list" active={pathname === "/"} />
-        </nav>
-        <div className="flex flex-col items-center">
-            <button className="font-delius p-4  bg-[#ADD3EA] rounded-3xl font-bold">log out</button>
-        </div>
+      <div className="flex flex-col items-center font-delius gap-4">
+        <Image src='/chiikawa.jpg' width={120} height={120} className="rounded-full border-4 border-[#4F84A5]" />
+        <p className="text-lg font-bold">username</p>
+        <p className="text-sm">edit profile</p>
+      </div>
+      <nav className="flex flex-col items-center w-full gap-4 font-delius">
+        <NavLink href="/" label="home" active={pathname === "/"} />
+        <NavLink href="/" label="write entry" active={pathname === "/"} />
+        <NavLink href="/" label="journal archive" active={pathname === "/"} />
+        <NavLink href="/" label="calendar" active={pathname === "/"} />
+        <NavLink href="/" label="task list" active={pathname === "/"} />
+        <NavLink href="/" label="shop" active={pathname === "/"} />
+        <NavLink href="/" label="gacha" active={pathname === "/"} />
+        <NavLink href="/" label="dashboard" active={pathname === "/"} />
+        <NavLink href="/" label="inventory" active={pathname === "/"} />
+        <NavLink href="/" label="item list" active={pathname === "/"} />
+      </nav>
+      <div className="flex flex-col items-center">
+        <button className="font-delius p-4  bg-[#ADD3EA] rounded-3xl font-bold" onClick={signOut}>Log Out</button>
+      </div>
     </div>
   );
 }
