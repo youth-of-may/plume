e0e9a31555956144f5ee847fff073c0312e46355
ellyyy-son <ellysonolegario@gmail.com>
2026-03-01 17:02:11 +0800

    Set up fonts and did NavBar

---
+++ components/navbar.tsx
@@ -0,0 +1,59 @@
+"use client"
+
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from 'react';
+
+export default function Navbar() {
+  return (
+    <>
+      <div className="w-[15%]">
+        <FullNav />
+      </div>
+    </>
+  );
+}
+
+import { usePathname } from "next/navigation";
+
+/// FullNav
+function FullNav() {
+  const pathname = usePathname();
+
+  return (
+    <div className="flex flex-col h-screen bg-[#F7F9FC] shadow-sm border-r-12 border-r-[#ADD3EA] pt-8 pb-8 gap-12">
+        <div className="flex flex-col items-center font-delius gap-4">
+            <Image src='/chiikawa.jpg' width={120} height={120} className="rounded-full border-4 border-[#4F84A5]"/>
+            <p className="text-lg font-bold">username</p>
+            <p className="text-sm">edit profile</p>
+        </div>
+        <nav className="flex flex-col items-center w-full gap-4 font-delius">
+            <NavLink href="/" label="home" active={pathname === "/"} />
+            <NavLink href="/" label="write entry" active={pathname === "/"} />
+            <NavLink href="/" label="journal archive" active={pathname === "/"} />
+            <NavLink href="/" label="calendar" active={pathname === "/"} />
+            <NavLink href="/" label="task list" active={pathname === "/"} />
+            <NavLink href="/" label="shop" active={pathname === "/"} />
+            <NavLink href="/" label="gacha" active={pathname === "/"} />
+            <NavLink href="/" label="dashboard" active={pathname === "/"} />
+            <NavLink href="/" label="inventory" active={pathname === "/"} />
+            <NavLink href="/" label="item list" active={pathname === "/"} />
+        </nav>
+        <div className="flex flex-col items-center">
+            <button className="font-delius p-4  bg-[#ADD3EA] rounded-3xl font-bold">log out</button>
+        </div>
+    </div>
+  );
+}
+
+// Helper component to remove repetitive code and remove fixed widths
+function NavLink({ href, label, active }: { href: string, label: string, active: boolean }) {
+  return (
+    <Link
+      href={href}
+      className={"flex items-center text-md font-bold transition-colors text-black hover:underline underline-offset-4"}
+    >
+      {label}
+    </Link>
+  );
+}
