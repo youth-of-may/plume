680abe8c951e91f1365442b1c40648c3131c99b2
ellyyy-son <ellysonolegario@gmail.com>
2026-03-02 19:38:21 +0800

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -55,26 +55,27 @@ function FullNav() {
 
   return (
     <div className="flex flex-col h-screen bg-[#F7F9FC] shadow-sm border-r-12 border-r-[#ADD3EA] pt-8 pb-8 gap-12">
-        <div className="flex flex-col items-center font-delius gap-4">
-            <Image src='/chiikawa.jpg' width={120} height={120} alt='Profile Picture' className="rounded-full border-4 border-[#4F84A5]"/>
-            <p className="text-lg font-bold">username</p>
-            <p className="text-sm">edit profile</p>
-        </div>
-        <nav className="flex flex-col items-center w-full gap-4 font-delius">
-            <NavLink href="/" label="home" active={pathname === "/"} />
-            <NavLink href="/journal" label="write entry" active={pathname === "/"} />
-            <NavLink href="/journal" label="journal archive" active={pathname === "/"} />
-            <NavLink href="/" label="calendar" active={pathname === "/"} />
-            <NavLink href="/tasks" label="task list" active={pathname === "/"} />
-            <NavLink href="/" label="shop" active={pathname === "/"} />
-            <NavLink href="/" label="gacha" active={pathname === "/"} />
-            <NavLink href="/" label="dashboard" active={pathname === "/"} />
-            <NavLink href="/" label="inventory" active={pathname === "/"} />
-            <NavLink href="/" label="item list" active={pathname === "/"} />
-        </nav>
-        <div className="flex flex-col items-center">
-            <button className="font-delius p-4  bg-[#ADD3EA] rounded-3xl font-bold">log out</button>
-        </div>
+      <div className="flex flex-col items-center font-delius gap-4">
+          <Image src='/chiikawa.jpg' width={120} height={120} alt='Profile Picture' className="rounded-full border-4 border-[#4F84A5]"/>
+          <p className="text-lg font-bold">username</p>
+           {isUser && <Link href="/profile/edit"><p className="text-sm">edit profile</p></Link>}
+      </div>
+      <nav className="flex flex-col items-center w-full gap-4 font-delius">
+          <NavLink href="/" label="home" active={pathname === "/"} />
+          <NavLink href="/journal" label="write entry" active={pathname === "/"} />
+          <NavLink href="/journal" label="journal archive" active={pathname === "/"} />
+          <NavLink href="/" label="calendar" active={pathname === "/"} />
+          <NavLink href="/tasks" label="task list" active={pathname === "/"} />
+          <NavLink href="/" label="shop" active={pathname === "/"} />
+          <NavLink href="/" label="gacha" active={pathname === "/"} />
+          <NavLink href="/" label="dashboard" active={pathname === "/"} />
+          <NavLink href="/" label="inventory" active={pathname === "/"} />
+          <NavLink href="/" label="item list" active={pathname === "/"} />
+      </nav>
+      <div className="flex flex-col items-center">
+        {isUser ? <button className="font-delius p-4  bg-[#ADD3EA] rounded-3xl font-bold" onClick={signOut}>Log Out</button> : 
+        <Link className="font-delius p-4  bg-[#ADD3EA] rounded-3xl font-bold" href="/login">Login</Link>}
+      </div>
     </div>
   );
 }
