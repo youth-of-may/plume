de4b00fe6100772e081417e2e554375e43d84843
ellyyy-son <ellysonolegario@gmail.com>
2026-03-15 23:29:04 +0800

    Update page.tsx

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -52,9 +52,8 @@ export default function Login() {
                 <div className="grid grid-rows-2 w-1/1 py-4 px-3">
                     <label htmlFor="email" className="self-center block font-delius text-xl/6 font-bold text-[#2E2805] basis-1/3">E-mail</label>
                     <div className="rounded-t-md bg-white wopacity-70 px-3 py-1 border-b-3 border-[#CCC38D] focus-within:border-[#F0B6CF]">
-                        <input id="email" type="email" name="new-password" placeholder="type your e-mail" className="font-delius block min-w-0 grow bg-transparent py-1.5 pr-3 pl-1 text-base text-[#2E2805] placeholder:text-[#CCC38D] focus:outline-none sm:text-sm/6"
+                        <input id="email" type="email" name="new-password" placeholder="E-mail" className="font-delius block min-w-0 grow bg-transparent py-1.5 pr-3 pl-1 text-base text-[#2E2805] placeholder:text-[#CCC38D] focus:outline-none sm:text-sm/6"
                             value={email}
-                            placeholder="Email"
                             onChange={(e) => setEmail(e.target.value)}
                             required
                         />
