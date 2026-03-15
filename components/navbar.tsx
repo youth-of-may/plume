56530f8d70effe83b87bc15ed5c75d1c2a7c53b6
ellyyy-son <ellysonolegario@gmail.com>
2026-03-15 19:12:32 +0800

    fixed navbar and changed journal to write

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -121,27 +121,31 @@ function FullNav() {
   }
 
   return (
-    <div className="flex flex-col h-screen overflow-auto bg-[#F7F9FC] shadow-sm border-r-12 border-r-[#ADD3EA] pt-8 pb-8 gap-12">
+    <div className="w-[15%] shrink-0 top-0 sticky h-screen">
+      <div className="flex flex-col h-screen bg-[#F7F9FC] shadow-sm border-r-12 border-r-[#ADD3EA] pt-8 pb-8 gap-12">
         <div className="flex flex-col items-center font-delius gap-4">
-          <Image src='/chiikawa.jpg' width={120} height={120} alt='Profile Picture' className="rounded-full border-4 border-[#4F84A5]"/>
-          <p className="text-lg font-bold">username</p>
-           {isUser && <Link href="/profile/edit"><p className="text-sm">edit profile</p></Link>}
-      </div>
-      <nav className="flex flex-col items-center w-full gap-4 font-delius">
-          <NavLink href="/" label="home" active={pathname === "/"} />
-          <NavLink href="/journal" label="write entry" active={pathname === "/"} />
-          <NavLink href="/archive" label="journal archive" active={pathname === "/"} />
-          <NavLink href="/" label="calendar" active={pathname === "/"} />
-          <NavLink href="/tasks" label="task list" active={pathname === "/"} />
-          <NavLink href="/" label="shop" active={pathname === "/"} />
-          <NavLink href="/" label="gacha" active={pathname === "/"} />
-          <NavLink href="/" label="dashboard" active={pathname === "/"} />
-          <NavLink href="/" label="inventory" active={pathname === "/"} />
-          <NavLink href="/" label="item list" active={pathname === "/"} />
-      </nav>
-      <div className="flex flex-col items-center">
-        {isUser ? <button className="font-delius p-4  bg-[#ADD3EA] rounded-3xl font-bold" onClick={signOut}>Log Out</button> : 
-        <Link className="font-delius p-4  bg-[#ADD3EA] rounded-3xl font-bold" href="/login">Login</Link>}
+          <Image src={dpURL} width={120} height={120} alt='Profile Picture' className="rounded-full border-4 border-[#4F84A5]"/>
+          <div className="flex flex-col gap-2 items-center">
+            <p className="text-lg font-bold">{username || "user"}</p>
+            <p className="text-md font-bold rounded-2xl border-2 border-[#4F84A5]"><span className="m-4">EXP: {exp} </span></p>
+          </div>
+          <Link href="/profile/edit"><p className="text-sm">edit profile</p></Link>
+        </div>
+        <nav className="flex flex-col items-center w-full gap-4 font-delius">
+          <NavLink href="/" label="home" />
+          <NavLink href="/write" label="write entry" />
+          <NavLink href="/archive" label="journal archive" />
+          <NavLink href="/calendar" label="calendar" />
+          <NavLink href="/tasks" label="task list" />
+          <NavLink href="/" label="shop" />
+          <NavLink href="/" label="gacha" />
+          <NavLink href="/" label="dashboard" />
+          <NavLink href="/inventory" label="inventory" />
+          <NavLink href="/" label="item list" />
+        </nav>
+        <div className="flex flex-col items-center">
+          <button className="font-delius p-4 bg-[#ADD3EA] rounded-3xl font-bold" onClick={signOut}>Log Out</button>
+        </div>
       </div>
     </div>
   );
