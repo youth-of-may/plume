4e0ab78c715f39610adebbd94f218ffaccd215cb
bea-ndc <beatrice.decastro@student.ateneo.edu>
2026-03-15 14:20:10 +0800

    Updated appearance of pet selection
    Updated the background behind the pet images for the pet selection page

--- app/(auth)/pet-selection/page.tsx
+++ app/(auth)/pet-selection/page.tsx
@@ -1,7 +1,6 @@
 import { createClient } from "@/utils/supabase/server";
 import Image from "next/image";
 import { cookies } from "next/headers";
-import Link from 'next/link';
 
 export async function getPetImages(){
     'use server'
@@ -38,30 +37,12 @@ export default async function petselection(){
     return (
     <div className={"bg-[#FBF5D1] border-5 border-[#E4DCAB] grid grid-template-rows-2 h-150 w-250 pt-10 px-10 pb-20 rounded-4xl justify-items-center translate-x-10 translate-y-15 shadow-xl/40 overflow-hidden"}>
             <h1 className="font-cherry text-[#2E2805] text-6xl pb-10">CHOOSE YOUR PET</h1>
-            <div className="flex gap-15 justify-center">
+            <div className="flex gap-15 justify-center bg-[#fef5ffbb] py-6 rounded-4xl shadow-lg">
                 {pets?.map((pet) => (
                     <div key={pet.pet_id} className="grid grid-template-rows-2 place-items-center gap-8">
                         <Image src={pet.pet_model} width={500} height={500} alt={pet.pet_type} /><h3 className="font-delius text-3xl">{pet.pet_type}</h3>
                     </div>
                 ))}
-            
-                 {/* <div className="grid grid-template-rows-2 place-items-center gap-8">
-                     <a><img src="https://picsum.photos/200" alt="Hamster" className="hover:size-60"></img></a>
-                     <h3 className="font-delius text-3xl">Hamster</h3>
-                     </div>
-                 <div className="grid grid-template-rows-2 place-items-center gap-8">
-                     <a><img src="https://picsum.photos/200" alt="Cat" className="hover:size-60"></img></a>
-                     <h3 className="font-delius text-3xl">Cat</h3></div>
-                 <div className="grid grid-template-rows-2 place-items-center gap-8">
-                    <a><Image 
-                     src={bunnyUrl} 
-                     alt="Bunny"
-                     width={300} 
-                     height={300}> 
-                     </Image></a>
-                     <h3 className="font-delius text-3xl">Bunny</h3>
-                     </div> */}
-             
         </div>
         </div>
     );
