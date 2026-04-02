b9e668a4c9ab12f02395fe63001abc8aa645af7e
bea-ndc <beatrice.decastro@student.ateneo.edu>
2026-04-02 21:24:29 +0800

    Updated the journal archive detail page
    Created the appearance and layout of the page for the individual archive entries

--- app/archive/[id]/client.tsx
+++ app/archive/[id]/client.tsx
@@ -4,7 +4,7 @@ import { deleteEntry } from "./server";
 
 export default function DeleteButton({ id }: { id: string }) {
   return (
-    <button onClick={() => deleteEntry(id)}>
+    <button onClick={() => deleteEntry(id)} className="bg-[#ADD3EA] mx-95 p-1 rounded-2xl text-sm border-3 border-[#5e94b67d]">
       Delete
     </button>
   );
--- app/archive/[id]/page.tsx
+++ app/archive/[id]/page.tsx
@@ -14,10 +14,18 @@ export default async function Entry({ params }: { params: Promise<{ id: string }
   }
 
   return (
-    <div className="flex flex-col gap-4">
-      <h1>{entry.entry_title}</h1>
-      <h1>{entry.entry_text}</h1>
-      <DeleteButton id={id}/>
+    <div className="flex flex-col px-15 py-20 font-delius">
+      <h1
+          className="font-cherry font-black text-4xl bg-[#CCC38D] text-center py-2 px-10 rounded-t-xl border-x-4 border-t-4 border-[#CCC38D] shadow-md"
+        >
+          {entry.entry_title}
+        </h1>
+      <div className="flex flex-col gap-6 bg-[#FBF5D1] py-5 px-10 rounded-b-xl border-4 border-[#CCC38D] shadow-md">
+        <div className="overflow-y-auto max-h-70 bg-white rounded-xl p-5">
+          <h1>{entry.entry_text}</h1>
+        </div>
+        <DeleteButton id={id}/>
+      </div>
     </div>
   );
 }
