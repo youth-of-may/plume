1eb63a1622c25b0220f77c89d4c1a04f1c270b02
ellyyy-son <ellysonolegario@gmail.com>
2026-04-03 19:52:25 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
+      <body suppressHydrationWarning
         className={`${cherry.variable} ${delius.variable} flex flex-row antialiased`}
       >
         <Navbar />
